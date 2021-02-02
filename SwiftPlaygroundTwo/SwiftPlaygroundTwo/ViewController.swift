017b1a9d4bcbd207c9ff8e3391acc1e74329bf47
Erin Samson <esamson@hawaii.edu>
2021-02-02 10:25:23 -1000

    Completed 1-4

--- SwiftPlaygroundTwo/SwiftPlaygroundTwo/ViewController.swift
+++ SwiftPlaygroundTwo/SwiftPlaygroundTwo/ViewController.swift
@@ -7,16 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var str = "Hello, playground"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        var goodGrade = "I've been doing all my work"
-            if goodGrade == "I've been doing all my work"
-    }
+//SP2 Introduction to If/Else Statements (Part 1)
+//Class 7 Feb 2
 
+//So, if/else statements allows our programs to make decisions based on whether the conditional statement evaluates to true OR false.  Based on what the conditional evaluates to, code executes in the if code block or if false, code executes in the else block.
 
+//Part 2
+
+var goodGrade = "I've been doing all my work"
+
+if goodGrade == "I've been doing all my work" //evaluate to false therefore skips over the if block and executes the else block
+{
+  //if block
+    print("I should be getting an A at this point")
+}
+//Part 3
+else
+{
+    //else block
+    print("I need to work harder")
 }
 
+//Part 4
+
+var gradeA = true
+
+//create and if/else statement on your own
+if gradeA == true
+{
+    print("I really like this class")
+
+}
+else
+{
+    print("I'm not so sure about coding in Swift")
+}
+//Part 5
