323457093ff89bafdabb7fd3ae27777ed7b6e6d3
Erin Samson <esamson@hawaii.edu>
2021-02-02 09:27:51 -1000

    SwiftPlaygroundTwo project

---
+++ SwiftPlaygroundTwo/SwiftPlaygroundTwo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftPlaygroundTwo
+//
+//  Created by Erin Samson on 2/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var goodGrade = "I've been doing all my work"
+            if goodGrade == "I've been doing all my work"
+    }
+
+
+}
+
